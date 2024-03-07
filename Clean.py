83e5d64422d6283f52fd0650c1a37c0cc039436b
nzy129 <nzy129@163.com>
2024-03-07 10:41:42 -0500

    Price Dispersion
    Added price dispersion

--- Clean.py
+++ Clean.py
@@ -3,23 +3,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# this file clean the data
-
-def clean_data(y:int, q:int):
+# this file clearn the data
+# y =2019
+# q=1
+def clean_data(y: int, q: int):
     import pandas as pd
     import zipfile
 
-    # with zipfile.ZipFile("Coupon_{}_{}.zip".format(y, q)) as z:
+    # with zipfile.ZipFile("Data/Coupon_{}_{}.zip".format(y, q)) as z:
     #    df_coupon = pd.read_csv(z.open('Origin_and_Destination_Survey_DB1BCoupon_{}_{}.csv'.format(2019, 1)), header=0)
     #    df_coupon = df_coupon[
     #        ["ItinID", "MktID", "SeqNum", "Origin", "Dest", "Break", "TkCarrier", "OpCarrier", "RPCarrier"]]
 
-    with zipfile.ZipFile("Ticket_{}_{}.zip".format(y, q)) as z:
+    with zipfile.ZipFile("Data/Ticket_{}_{}.zip".format(y, q)) as z:
         df_ticket = pd.read_csv(z.open('Origin_and_Destination_Survey_DB1BTicket_{}_{}.csv'.format(y, q)), header=0)
         df_ticket = df_ticket[["Origin", "ItinID", "Coupons", "Year", "Quarter", "RoundTrip", "OnLine",
                                "Passengers", "ItinFare", "Distance"]]
 
-    with zipfile.ZipFile("Market_{}_{}.zip".format(y, q)) as z:
+    with zipfile.ZipFile("Data/Market_{}_{}.zip".format(y, q)) as z:
         df_market = pd.read_csv(z.open('Origin_and_Destination_Survey_DB1BMarket_{}_{}.csv'.format(y, q)), header=0)
 
         df_market = df_market[["ItinID", "MktID", "MktCoupons", "Year", "Quarter", "Origin", "Dest", "AirportGroup",
@@ -34,13 +35,17 @@ def clean_data(y:int, q:int):
 
     # 97.7% trips have the fewer than or equal to 4 tickets
     # print(len(df_joined[df_joined.Coupons <= 4].ItinID.unique()) / len(df_joined.ItinID.unique()))
+
+    # 99.9% trips have fewer than or equal to 6 tickets
+    # print(len(df_joined[df_joined.Coupons <= 6].ItinID.unique()) / len(df_joined.ItinID.unique()))
+
     # print(len(df_joined[(df_joined.RoundTrip == 0) & (df_joined.Coupons == 3)].ItinID.unique()) / len(df_joined.ItinID.unique()))
 
     df_joined["market"] = df_joined.Origin_m + ":" + df_joined.Dest_m
     df_joined["TTFare"] = df_joined.MktFare * df_joined.Passengers
     df_test = df_joined[["market", "TkCarrierGroup", "OpCarrierGroup", "AirportGroup",
                          "RoundTrip", "OnLine", "MktCoupons", "Coupons", "Passengers", "Year", "Quarter", "TTFare",
-                         "ItinFare", "MktDistance", "NonStopMiles"]]
+                         "ItinFare", "MktDistance", "NonStopMiles", "MktFare"]]
 
     # output = df_coupon["OpCarrier"].value_counts()
     # output = df_coupon["TkCarrier"].value_counts()
@@ -61,10 +66,22 @@ def clean_data(y:int, q:int):
         return 1 if len(set(value.split(":"))) == 1 else 0
 
     df_test_new = df_test.copy()  # to get rid of warning
+    df_test_new["Nonstop"] = 1 * (df_test_new["AirportGroup"].apply(lambda x: len(x.split(":"))) == 2)
+
+    # find the codeshare products between JetBlue and AA
+    df_test_new["B6AA"] = 1 * df_test_new.loc[:, "OpCarrierGroup"].str.contains("B6") * df_test_new.loc[:,
+                                                                                        "OpCarrierGroup"].str.contains(
+        "AA")
+    # the city pair where they do codeshare
+    df_test_new['NEA_market'] = 1 * (df_test_new.groupby("market")['B6AA'].transform('sum') > 0)
+    df_test_new['NEA_market'] = df_test_new['NEA_market'].astype(int)
+    df_test_new['NEA_market_codeshared'] = 1 * (
+                df_test_new.groupby(["market", "Year", "Quarter"])['B6AA'].transform('sum') > 0)
 
     df_test_new.loc[:, "OnLine_new"] = df_test_new.loc[:, "OpCarrierGroup"].apply(check_unique_op)
 
     df_test_new = df_test_new[(df_test_new.ItinFare < 2500) & (df_test.ItinFare >= 20)]
+
     # df_test.drop(["OnLine","MktCoupons","Coupons"], axis=1, inplace=True)
 
     output = df_test_new.groupby(
@@ -75,7 +92,8 @@ def clean_data(y:int, q:int):
     output['AveFare'] = output["TTFare"] / output["Passengers"]
 
     output['total_quantity'] = output.groupby("market")['Passengers'].transform('sum')
-    cleaned_data = output[output['total_quantity'] >= 100]
+    cleaned_data = output[output['total_quantity'] >= 1]  # change it to 100 to delete low demand market
+
     # around 0.76% of the passengers contains  --
 
     # print(len(cleaned_data.market.unique())) # 10710 markets
@@ -88,4 +106,46 @@ def clean_data(y:int, q:int):
     # plt.xlabel('Total Quantity')
     # plt.ylabel('Frequency')
     # plt.show()
-    return cleaned_data
+
+    def q1(x, weights):
+        sorted_indices = np.argsort(x)
+        sorted_weights = weights.iloc[sorted_indices]
+        sorted_values = x.iloc[sorted_indices]
+        cumulative_weights = np.cumsum(sorted_weights)
+        percentile = np.searchsorted(cumulative_weights, 0.1 * cumulative_weights.iloc[-1])
+        return sorted_values.iloc[percentile]
+
+    def q9(x, weights):
+        sorted_indices = np.argsort(x)
+        sorted_weights = weights.iloc[sorted_indices]
+        sorted_values = x.iloc[sorted_indices]
+        cumulative_weights = np.cumsum(sorted_weights)
+        percentile = np.searchsorted(cumulative_weights, 0.9 * cumulative_weights.iloc[-1])
+        return sorted_values.iloc[percentile]
+
+    def variance_weighted(x, weights):
+        weighted_mean = np.average(x, weights=weights)
+        variance = np.average((x - weighted_mean) ** 2, weights=weights)
+        return variance
+
+    def average_weighted(x, weights):
+        weighted_mean = np.average(x, weights=weights)
+        return weighted_mean
+
+    f = {'MktFare': [
+        ('mmktfare', lambda x: average_weighted(x, df_test_new.loc[x.index, 'Passengers'])),
+        ('vmktfare', lambda x: variance_weighted(x, df_test_new.loc[x.index, 'Passengers'])),
+        ('q1fare', lambda x: q1(x, df_test_new.loc[x.index, 'Passengers'])),
+        ('q9fare', lambda x: q9(x, df_test_new.loc[x.index, 'Passengers']))],
+        'RoundTrip': [lambda x: average_weighted(x, df_test_new.loc[x.index, 'Passengers'])],
+        'MktDistance': [lambda x: average_weighted(x, df_test_new.loc[x.index, 'Passengers'])],
+        'Nonstop': [lambda x: average_weighted(x, df_test_new.loc[x.index, 'Passengers'])],
+        'Passengers': [lambda x: sum(x)]
+    }
+
+    output2 = df_test_new.groupby(
+        by=["market", "NonStopMiles", "Year", "Quarter", "NEA_market", "NEA_market_codeshared"],
+        level=None).agg(f).reset_index()
+
+    output2['total_quantity'] = output2.groupby("market")['Passengers'].transform('sum')
+    return cleaned_data, output2
--- Gather_data.py
+++ Gather_data.py
@@ -5,15 +5,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 from Clean import clean_data
 df = pd.DataFrame()
+# y=2019
+# q=1
 # I don't have 2023Q4 data yet
 for y in [2019, 2020, 2021, 2022, 2023]:
     if y != 2023:
         for q in range(1, 5):
             output = clean_data(y, q)
-            df = pd.concat([df, output], ignore_index=True)
+
+            df = pd.concat([df, output[1]], ignore_index=True)
     else:
         for q in range(1, 4):
             output = clean_data(y, q)
-            df = pd.concat([df, output], ignore_index=True)
+            df = pd.concat([df, output[1]], ignore_index=True)
 
-df.to_csv('Cleaned_2019_2023.csv', index=False)
+df.to_csv('Cleaned_2019_2023_without_delete_small_market_dispersion.csv', index=False)
